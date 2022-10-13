7553f269624cd1d566fbbd6c69928abfc66a951c
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-13 03:46:49 -0500

    feat: add ChillRx Staking
    Signed-off-by: Sweets Sweetman <sweetmantech@gmail.com>

---
+++ src/adapters/chillrx.adapter.ts
@@ -0,0 +1,47 @@
+// Copyright Abridged, Inc. 2022. All Rights Reserved.
+// Node module: @collabland/staking-contracts
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {BindingScope, extensionFor, injectable} from '@loopback/core';
+import {BigNumber} from 'ethers';
+import {STAKING_ADAPTERS_EXTENSION_POINT} from '../keys';
+import {BaseStakingContractAdapter, StakingAsset} from '../staking';
+// Use the full path to import instead of `../types`
+import {ChillrxStaking__factory} from '../types/factories/ChillrxStaking__factory';
+
+@injectable(
+  {
+    scope: BindingScope.SINGLETON, // Mark the adapter as a singleton
+  },
+  // Mark it as an extension to staking contracts service
+  extensionFor(STAKING_ADAPTERS_EXTENSION_POINT),
+)
+export class ChillRxStakingContractAdapter extends BaseStakingContractAdapter {
+  /**
+   * The contract address
+   */
+  contractAddress = '0x3f553d76b78efb6B50b0022E89E1A60aA7800565';
+
+  /**
+   * Assets that can be staked to this contract
+   */
+  supportedAssets: StakingAsset[] = [
+    {
+      asset: 'ERC721:0xe7e07f9dff6b48eba32641c53816f25368297d22',
+    },
+  ];
+
+  /**
+   * Get staked token ids for the given owner
+   * @param owner - Owner address
+   * @returns
+   */
+  getStakedTokenIds(owner: string): Promise<BigNumber[]> {
+    const contract = ChillrxStaking__factory.connect(
+      this.contractAddress,
+      this.provider,
+    );
+    return contract.tokensOfOwner(owner);
+  }
+}

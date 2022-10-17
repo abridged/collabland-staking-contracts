8808417af1d1866bb3c3d7469ff458d8102c02f5
xiaodao <lychees67@gmail.com>
2022-10-17 12:42:53 +0800

    feat: add e4c adapter for e4c nft staking
    Signed-off-by: xiaodao <lychees67@gmail.com>

---
+++ src/adapters/e4c-ranger-staking.adapter.ts
@@ -0,0 +1,55 @@
+// Copyright Abridged, Inc. 2022. All Rights Reserved.
+// Node module: @collabland/staking-contracts
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {BindingScope, extensionFor, injectable} from '@loopback/core';
+import {Contract, Provider} from 'ethcall';
+import {BigNumber} from 'ethers';
+import {STAKING_ADAPTERS_EXTENSION_POINT} from '../keys';
+import {BaseStakingContractAdapter, StakingAsset} from '../staking';
+import {E4cRangerStaking__factory} from '../types/factories/E4cRangerStaking__factory';
+
+@injectable(
+  {
+    scope: BindingScope.SINGLETON,
+  },
+  extensionFor(STAKING_ADAPTERS_EXTENSION_POINT),
+)
+export class E4CRangerStakingContractAdapter extends BaseStakingContractAdapter {
+  contractAddress = '0x9c18beA91AE053397918410311dbB89295baE18b';
+
+  supportedAssets: StakingAsset[] = [
+    {
+      asset: 'ERC721:0xbA265B93519E6473F34F46ee35F4B23970F41a3f',
+    },
+  ];
+
+  ecProvider = new Provider();
+
+  constructor() {
+    super();
+    this.ecProvider.init(this.provider);
+  }
+
+  async getStakedTokenIds(owner: string): Promise<BigNumber[]> {
+    const contract = new Contract(this.contractAddress, E4cRangerStaking__factory.abi);
+
+    const items = new Array(650);
+    for (let i = 0; i < 650; i++) {
+      items[i] = i + 1;
+    }
+    const owners = await this.ecProvider.all(items.map(i => contract.originalOwner(i)));
+
+    const tokens: BigNumber[] = []
+    for (let i = 0; i < owners.length; i++) {
+      if (owners[i] === owner) tokens.push(BigNumber.from(i + 1));
+    }
+    return tokens;
+  }
+
+  async getStakedTokenBalance(owner: string): Promise<BigNumber> {
+    const Ids = await this.getStakedTokenIds(owner);
+    return BigNumber.from(Ids.length);
+  }
+}

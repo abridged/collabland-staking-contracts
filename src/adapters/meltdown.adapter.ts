ae735083728ef361145d24e65483a206d148de91
xenonlabsio <sagarjoisar16@gmail.com>
2023-03-27 01:47:26 +0530

    Add Meltdown03 Staking
    Signed-off-by: xenonlabsio <sagarjoisar16@gmail.com>

---
+++ src/adapters/meltdown.adapter.ts
@@ -0,0 +1,43 @@
+import {BindingScope, extensionFor, injectable} from '@loopback/core';
+import {BigNumber} from 'ethers';
+import {STAKING_ADAPTERS_EXTENSION_POINT} from '../keys';
+import {BaseStakingContractAdapter, StakingAsset} from '../staking';
+// Use the full path to import instead of `../types`
+import {Meltdown03__factory} from '../types/factories/Meltdown03__factory';
+
+@injectable(
+  {
+    scope: BindingScope.SINGLETON, // Mark the adapter as a singleton
+  },
+  // Mark it as an extension to staking contracts service
+  extensionFor(STAKING_ADAPTERS_EXTENSION_POINT),
+)
+export class Meltdown03ContractAdapter extends BaseStakingContractAdapter {
+  /**
+   * The contract address
+   */
+  contractAddress = '0x056afF8f6aE1F9f02EEbE270b7B14528EA143e2E';
+
+  /**
+   * Assets that can be staked to this contract
+   */
+  supportedAssets: StakingAsset[] = [
+    {
+      asset: 'ERC721:0x94355367a2d1896fed2Cd61CD7a11a7b50071451',
+    },
+  ];
+
+  /**
+   * Get staked token ids for the given owner
+   * @param owner - Owner address
+   * @returns
+   */
+  async getStakedTokenIds(owner: string): Promise<BigNumber[]> {
+    const contract = Meltdown03__factory.connect(
+      this.contractAddress,
+      this.provider,
+    );
+    const data = await contract.getStakeInfo(owner);
+    return data._tokensStaked;
+  }
+}

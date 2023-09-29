894c76af421a84ce388f9275813fb513ea38d5cb
Yagiz Evren <yevren@zynga.com>
2023-09-29 13:53:05 -0500

    Added sugartown staking adapter
    Signed-off-by: Yagiz Evren <yevren@zynga.com>

---
+++ src/adapters/sugartown.adapter.ts
@@ -0,0 +1,39 @@
+import {BindingScope, extensionFor, injectable} from '@loopback/core';
+import {BigNumber} from 'ethers';
+import {STAKING_ADAPTERS_EXTENSION_POINT} from '../keys';
+import {BaseStakingContractAdapter, StakingAsset} from '../staking';
+// Use the full path to import instead of `../types`
+import {SugartownStaking__factory} from '../types/factories/SugartownStaking__factory';
+
+@injectable(
+  {
+    scope: BindingScope.SINGLETON, // Mark the adapter as a singleton
+  },
+  // Mark it as an extension to staking contracts service
+  extensionFor(STAKING_ADAPTERS_EXTENSION_POINT),
+)
+export class SugartownStakingContractAdapter extends BaseStakingContractAdapter {
+  /**
+   * The contract address
+   */
+  contractAddress = '0xd564C25B760cb278a55bDD98831f4ff4B6C97B38';
+
+  /**
+   * Assets that can be staked to this contract
+   */
+  supportedAssets: StakingAsset[] = [
+    {
+      asset: 'ERC721:0xd564C25B760cb278a55bDD98831f4ff4B6C97B38',
+    },
+  ];
+
+  /**
+   * Get staked token balance for the given owner
+   * @param owner - Owner address
+   * @returns
+   */
+  getStakedTokenBalance(owner: string): Promise<BigNumber> {
+    const contract = SugartownStaking__factory.connect(this.contractAddress, this.provider);
+    return contract.getUserStakedTokenCount(owner);
+  }
+}

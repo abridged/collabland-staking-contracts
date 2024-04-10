c59a0a2da3522e31b67473954088ff006901d1b8
dr-noid <67706284+dr-noid@users.noreply.github.com>
2024-04-10 20:24:25 +0200

    Add Blastopians staking contract (#200)
    Signed-off-by: dr-noid <creedalpha@gmail.com>
    Signed-off-by: Raymond Feng <raymond@collab.land>
    Co-authored-by: dr-noid <creedalpha@gmail.com>
    Co-authored-by: Raymond Feng <raymond@collab.land>

--- src/component.ts
+++ src/component.ts
@@ -28,6 +28,7 @@ import {Meltdown03ContractAdapter} from './adapters/meltdown.adapter.js';
 import {AngelBlockStakingContractAdapter} from './adapters/ab.adapter.js';
 import {BansheesStakingContractAdapter} from './adapters/banshees-staking.adapter.js';
 import {BapeliensStakingContractAdapter} from './adapters/bapeliens-staking.adapter.js';
+import {BlastopiansStakingContractAdapter} from './adapters/blastopians.adapter.js';
 import {BibizStakingContractAdapter} from './adapters/bibiz.adapter.js';
 import {CbzGrowStakingContractAdapter} from './adapters/cbzgrow.adapter.js';
 import {
@@ -122,6 +123,7 @@ export class StakingContractsComponent implements Component {
     SupremeKong2StakingContractAdapter,
     SugartownStakingContractAdapter,
     BansheesStakingContractAdapter,
+    BlastopiansStakingContractAdapter,
     FlooringProtocolStakingContractAdapter,
     BibizStakingContractAdapter,
     HabibizRoyalsStakingContractAdapter,

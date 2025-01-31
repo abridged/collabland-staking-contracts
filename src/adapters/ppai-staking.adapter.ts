25b0a0b07f8ecdd24eaf5c5bb24bf85413c850cd
erendeno <erendeno@profit-pilot.ai>
2025-01-31 19:24:36 +0530

    Add PPAI staking
    Signed-off-by: erendeno <erendeno@profit-pilot.ai>

--- src/adapters/ppai-staking.adapter.ts
+++ src/adapters/ppai-staking.adapter.ts
@@ -23,23 +23,37 @@ export class PpaiStakingContractAdapter extends BaseStakingContractAdapter {
 
   supportedAssets: StakingAsset[] = [
     {
+      name: 'PPAI',
       asset: 'ERC20:0x2972F737450EBb29434d4102CAF0880c0e768292',
+    },
+    {
+      name: 'PPAI-WETH-UNIV2',
+      asset: 'ERC20:0xf0C1997bAdcE2140713C535f8190E9768CbF8F4C',
     },
   ];
 
-  async getStakedTokenBalance(owner: string): Promise<BigNumber> {
+  async getStakedTokenBalance(
+    owner: string,
+    name?: string,
+  ): Promise<BigNumber> {
     const stakingContract = PpaiStaking__factory.connect(
       this.contractAddress,
       this.provider,
     );
-    const tokenContract = PpaiToken__factory.connect(
-      this.tokenAddress,
-      this.provider,
-    );
 
-    const stakingInfo = await stakingContract.userInfo(0, owner);
-    const membershipAmount = await tokenContract.getMembership(owner);
+    if (name === 'PPAI-WETH-UNIV2') {
+      const stakingInfo = await stakingContract.userInfo(1, owner);
+      return stakingInfo.amount;
+    } else {
+      const tokenContract = PpaiToken__factory.connect(
+        this.tokenAddress,
+        this.provider,
+      );
 
-    return stakingInfo.amount.add(membershipAmount);
+      const stakingInfo = await stakingContract.userInfo(0, owner);
+      const membershipAmount = await tokenContract.getMembership(owner);
+
+      return stakingInfo.amount.add(membershipAmount);
+    }
   }
 }
